13d74fb70f16c37d7380e60de2ce676638d1013b
sirishkumar <sirishkumar@gmail.com>
2022-01-23 15:46:50 +0000

    Added terraform code
    Added terraform code
    - Created GCS bucket
    - Updated .gitignore to ignore all state and env variables

---
+++ week_1/terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.1.4"
+  backend "gcs" {
+    bucket = "tf-state-prod.de-zoomcamp.bethala.net"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+    project = var.project
+    region  = var.region
+}
+
+# Data Lake Bucket
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "data-lake" {
+    name = "${local.data_lake_bucket}_${var.project}"
+    location = var.region
+
+    storage_class = var.storage_class
+    uniform_bucket_level_access = true // Use IAM policies to control access to this bucket
+
+    versioning {
+        enabled = true
+    }
+
+    lifecycle_rule {
+        action {
+            type = "Delete"
+        }
+        condition {
+            age = 90
+        }
+    }
+
+    force_destroy = true
+}
+
+
---
+++ week_1/terraform/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "dtc_data_lake"
+}
+
+variable terraform_tf_state_bucket {
+  type = string
+  default = "Bucket to save terraform state"
+}
+
+variable project {
+  type        = string
+  description = "GCP Project ID"
+}
+
+variable region {
+  type        = string
+  description = "GCP Region"
+}
+
+variable storage_class {
+    type        = string
+    default     = "STANDARD"
+    description = "GCP bucket storage class"
+}
