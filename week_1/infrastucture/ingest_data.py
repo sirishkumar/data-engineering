5d5c5f60f617cebc9ede528723313b072fd74e15
Sirish Kumar <sirishkumar@gmail.com>
2022-01-24 12:48:13 +0100

    ingest pipeline

---
+++ week_1/infrastucture/ingest_data.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    while True: 
+        t_start = time()
+
+        df = next(df_iter)
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', default="root", help='user name for postgres')
+    parser.add_argument('--password', default="root", help='password for postgres')
+    parser.add_argument('--host', default="localhost", help='host for postgres')
+    parser.add_argument('--port', default="5432", help='port for postgres')
+    parser.add_argument('--db', default="ny_taxi", help='database name for postgres')
+    parser.add_argument('--table_name', default="yello_taxi_trips", help='name of the table where we will write the results to')
+    parser.add_argument('--url', default="https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv", help='url of the csv file')
+
+    args = parser.parse_args()
+
+    main(args)
